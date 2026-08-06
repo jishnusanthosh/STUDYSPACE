783203d6959c0385cc7a7953e7ace4acabc5fbc0
Jishnu Santhosh <jishnusanthosh321@gmail.com>
2026-08-06 22:17:53 +0530

    callback

---
+++ JAVASCRIPT/CODE MALAYALAM/arrow.js
@@ -0,0 +1,61 @@
+// const values=[1,2,3,4,5,6,7,8]
+
+// values.forEach((val)=>{
+//     console.log(val);
+    
+// })
+
+// const fn=(a,b)=>{
+//     return a+b
+// }
+// console.log(fn(3,4));
+
+
+// const fn=(a,b)=>a+b
+// console.log(fn(2,3));
+
+
+// const fn=a=>a+100;
+
+// console.log(fn(3));
+
+
+// const fn=()=>[2,2,4,6,7]
+
+// console.log(fn());
+
+
+
+// const obj={
+//     a:2,
+//     b:3
+// }
+
+
+// const fn= ()=>({
+//     a:2,
+//     b:3
+// })
+
+// console.log(fn());
+
+
+// const obj={
+//     a:100,
+//     b:200,
+//     myFun:function(){
+//         return this.a + this.b
+//     }
+// }
+// console.log(obj.myFun());
+
+// const obj={
+//     a:100,
+//     b:200,
+//     myFun:()=>{
+//         return this.a + this.b
+//     }
+// }
+// console.log(obj.myFun());   //oputput will be NaN
+
+
---
+++ JAVASCRIPT/CODE MALAYALAM/callaback.js
@@ -0,0 +1,23 @@
+// console.log("one");
+
+// document.getElementById('button').onclick=clickFunction;
+
+// // function clickFunction(arg) {
+// //     console.log(arg);
+
+// // }
+
+// clickFunction(10)
+// console.log('two');
+
+// console.log("first");
+
+// const a = [1, 3, 4, 6, 7];
+
+// a.forEach((arg) => {
+//   {
+//     console.log(arg);
+//   }
+// });
+
+// console.log("second");
